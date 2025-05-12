46f287c37eec6d620124fcd2a3934b4d2e294a79
thiagoluisbecker <thiagobeckerrocha@gmail.com>
2025-05-12 17:05:33 -0300

    filmes por genero

--- app/controllers/filme_controllers.py
+++ app/controllers/filme_controllers.py
@@ -7,6 +7,9 @@ filme_bp = Blueprint('filme', __name__)
 @filme_bp.route('/', methods=['GET'])
 def get_filmes():
     filmes = Filme.query.all()
+    if not filmes:
+        return jsonify({'mensagem': f'Nenhum filme encontrado'}), 404
+    
     lista_filmes = []
     for filme in filmes:
         lista_filmes.append({
@@ -25,6 +28,9 @@ def get_filmes():
 @filme_bp.route('/<int:filme_id>', methods=['GET'])
 def get_filme_por_id(filme_id):
     filme = Filme.query.get_or_404(filme_id)
+    if not filme:
+        return jsonify({'mensagem': f'Nenhum filme encontrado com o id {filme_id}'}), 404
+    
     dados_filme = {
         'id':filme.id,
         'nome':filme.nome,
@@ -38,4 +44,23 @@ def get_filme_por_id(filme_id):
 
 
 
-    
+@filme_bp.route('/genero/<genero>', methods=['GET'])
+def get_filmes_por_genero(genero):
+    filmes = Filme.query.filter_by(genero=genero).all()
+    if not filmes:
+        return jsonify({'mensagem': f'Nenhum filme encontrado no gênero: {genero}'}), 404
+
+
+    lista_filmes = []
+    for filme in filmes:
+        lista_filmes.append({
+            'id': filme.id,
+            'nome': filme.nome,
+            'genero': filme.genero,
+            'sinopse': filme.sinopse,
+            'diretor': filme.diretor,
+            'ano': filme.ano,
+        })
+
+
+    return jsonify(lista_filmes), 200
