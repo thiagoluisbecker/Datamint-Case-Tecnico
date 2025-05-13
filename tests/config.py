af1cf30f9c29e8526b37fd55220d75e7c018aae7
thiagoluisbecker <thiagobeckerrocha@gmail.com>
2025-05-13 13:02:43 -0300

    config e estrutura

---
+++ tests/config.py
@@ -0,0 +1,15 @@
+import pytest
+from app import create_app, db
+
+@pytest.fixture
+def app():
+    app = create_app({
+        'TESTING': True,
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
+        'SQLALCHEMY_TRACK_MODIFICATIONS': False
+    })
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
